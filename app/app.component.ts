16bae53ca8b79a768a3013151cf2de7e8284f099
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-25 12:59:30 +0300

    Add Bootstrap components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+    <navbar></navbar>
+    <jumbotron></jumbotron>
+    <div class="container">
+	    <router-outlet></router-outlet>
+	</div>
+  `
 })
 export class AppComponent { }
--- app/components/pages/about.component.js
+++ app/components/pages/about.component.js
@@ -9,17 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var AppComponent = (function () {
-    function AppComponent() {
+var AboutComponent = (function () {
+    function AboutComponent() {
     }
-    AppComponent = __decorate([
+    AboutComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            template: "\n    <navbar></navbar>\n    <jumbotron></jumbotron>\n    <div class=\"container\">\n\t    <router-outlet></router-outlet>\n\t</div>\n  "
+            moduleId: module.id,
+            selector: 'about',
+            templateUrl: 'about.component.html'
         }), 
         __metadata('design:paramtypes', [])
-    ], AppComponent);
-    return AppComponent;
+    ], AboutComponent);
+    return AboutComponent;
 }());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+exports.AboutComponent = AboutComponent;
+//# sourceMappingURL=about.component.js.map
